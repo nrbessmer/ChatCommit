d76d0afdfb0cd42c018889e1eb1eb5fa1b359138
nrbessmersmart <nicholas.bessmer@smartsheet.com>
2025-05-06 19:30:31 -0400

    🔄 Update and deploy latest changes

--- frontend/postcss.config.js
+++ frontend/postcss.config.js
@@ -1,6 +1,7 @@
+// postcss.config.js
 module.exports = {
   plugins: {
-    tailwindcss: {},               // ← the real Tailwind plugin
-    autoprefixer: {},
-  },
+    '@tailwindcss/postcss': {},   // ← new plugin
+    autoprefixer: {}
+  }
 }
